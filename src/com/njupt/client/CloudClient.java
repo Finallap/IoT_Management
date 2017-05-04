3d0009d5872040bce2c80a45fe104f79f9a88f79
Final_Lap <547805712@qq.com>
2017-05-04 17:25:40 +0800

    制作了LoginServlet

---
+++ src/com/njupt/client/CloudClient.java
@@ -0,0 +1,27 @@
+package com.njupt.client;
+
+import com.njupt.service.ServiceDelegate;
+import com.njupt.service.ServiceService;
+
+public class CloudClient {
+	public ServiceDelegate client;
+	
+//	static {
+//		ServiceService service = new ServiceService();	
+//		client = service.getServicePort();
+//	}
+	
+	 private CloudClient() {
+		ServiceService service = new ServiceService();	
+		client = service.getServicePort();
+	 }
+	 
+	 private static CloudClient single = null;  
+	 //静态工厂方法   
+	 public static CloudClient getInstance() {  
+	      if (single == null) {    
+	          single = new CloudClient();  
+	      }    
+	      return single;  
+	 }  
+}
--- src/com/webclient/test/ClientTest.java
+++ src/com/webclient/test/ClientTest.java
@@ -1,7 +1,8 @@
 package com.webclient.test;
 
-import client.DaoDelegate;
-import client.DaoService;
+import com.njupt.service.ServiceDelegate;
+import com.njupt.service.ServiceService;
+
 
 
 
@@ -9,12 +10,12 @@ public class ClientTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		DaoService ds = new DaoService();	
-		DaoDelegate dd= ds.getDaoPort();
-		dd.insertstudent("sss");
-		String s = dd.sayHello();
-		System.out.println(s);
-		System.out.println(dd.getStudentnamebyID(2));
+		ServiceService ds = new ServiceService();	
+		ServiceDelegate dd= ds.getServicePort();
+//		dd.insertstudent("sss");
+//		String s = dd.sayHello();
+//		System.out.println(s);
+//		System.out.println(dd.getStudentnamebyID(1));
 	}
 
 }
