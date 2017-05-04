ae63f3e1a04f1900b53ae0970065e9b71bc0c8e9
Final_Lap <547805712@qq.com>
2017-05-04 23:26:08 +0800

    增加Filter，但过滤不正常

---
+++ src/com/njupt/filter/LoginFilter.java
@@ -0,0 +1,64 @@
+package com.njupt.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+@WebFilter(filterName="LoginFilter",urlPatterns="/*")
+public class LoginFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public LoginFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		HttpServletRequest http_request = (HttpServletRequest)request;
+		HttpServletResponse http_response = (HttpServletResponse)response;
+		
+		HttpSession session = http_request.getSession();
+		
+		if(http_request.getRequestURI().indexOf("login.jsp")!=-1)
+			chain.doFilter(request, response);
+		else
+		{
+			if(session.getAttribute("username")==null)
+				chain.doFilter(request, response);
+			else
+				http_response.sendRedirect(http_request.getContextPath()+"/login.jsp");
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
